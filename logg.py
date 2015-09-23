65736db7b817dfd675bd260f6371197ee79026a0
Ole A. Fauske <o_fauske@hotmail.com>
2015-09-23 13:57:47 +0200

    Created new write function

--- logg.py
+++ logg.py
@@ -1,25 +1,23 @@
 from time import strftime
 import os.path
 
-def inputSYS(input):
-	loggfil='/var/log/raspiGrow.log'
-	if not os.path.isfile(loggfil):
-		with open(loggfil, 'a') as f:
-			f.write("|--- Log created {!s}\n".format(strftime("%d.%m.%y %H:%M ---| ")))
-	with open(loggfil, 'a') as f:
+def write(file, input, check=False):
+	if not os.path.isfile(file):
+		with open(file, 'a') as f:
+			f.write("|--- Log created {!s}\n".format(strftime("%d.%m.%y %H:%M ---|")))
+			if check:
+				f.write("{!s}\n".format("Time, Intake, Water, Exhaust"))
+	with open(file, 'a') as f:
 		f.write("{!s} {!s}\n".format(strftime("%H:%M "), input))
 	f.close()
 
-def inputTMP(input):
-	loggfil='/var/log/tmp.log'
-	if not os.path.isfile(loggfil):
-		with open(loggfil, 'a') as f:
-			f.write("|--- Log created {!s}\n".format(strftime("%d.%m.%y %H:%M ---| ")))
-			f.write("{!s}\n".format("Time, Intake, Water, Exhaust") )
+def inputSYS(string):
+	logfile='/var/log/raspiGrow.log'
+	write(logfile, string)
 
-	with open(loggfil, 'a') as f:
-		f.write("{!s} {!s}\n".format(strftime("%H:%M "), input))
-	f.close()
+def inputTMP(string):
+	logfile='/var/log/temp.log'
+	write(logfile, string, check=True)
 
 def main():
 	exit()
